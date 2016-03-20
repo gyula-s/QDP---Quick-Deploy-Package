f79f2376c722b8fb271e07c20dab58810a511773
Gyula Soos <soo14145300@student.citybathcoll.ac.uk>
2016-03-20 09:36:04 +0000

    created a new element in top of site
    displays currently logged in user, and logout(with some limitations)

---
+++ admin/loginState.php
@@ -0,0 +1,16 @@
+<?php
+echo "Hello ".$_SERVER['PHP_AUTH_USER'];
+?>
+<br />
+<a href="../">Preview site</a>
+<br />
+
+<a href="#" onclick="logout();">Log out</a>
+
+<script type="text/javascript">
+function logout() {
+	
+	alert("Hello\nI will deffinitely try to log you out now,\nbut you can't promise anything due to limitations in the apache webserver.\n\nYou should restart your browser, and then test if it worked.\nIf not, a computer restart will deffinitely help!");
+	javascript:document.location=document.URL.replace('://','://logout@');
+}
+</script>
