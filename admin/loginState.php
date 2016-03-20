da2ad21f4b40d9551c73c8978e18cd6911506722
Gyula Soos <soo14145300@student.citybathcoll.ac.uk>
2016-03-20 12:50:52 +0000

    fixed typo

--- admin/loginState.php
+++ admin/loginState.php
@@ -10,7 +10,7 @@ echo "Hello ".$_SERVER['PHP_AUTH_USER'];
 <script type="text/javascript">
 function logout() {
 	
-	alert("Hello\nI will deffinitely try to log you out now,\nbut you can't promise anything due to limitations in the apache webserver.\n\nYou should restart your browser, and then test if it worked.\nIf not, a computer restart will deffinitely help!");
+	alert("Hello\nI will deffinitely try to log you out now,\nbut I can't promise anything due to limitations in the apache webserver.\n\nYou should restart your browser, and then test if it worked.\nIf not, a computer restart will deffinitely help!");
 	javascript:document.location=document.URL.replace('://','://logout@');
 }
 </script>
